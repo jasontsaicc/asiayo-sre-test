f0d7d51a7af6957949495aa27f3fd581e783f303
JasonTsai <jason.tsaicc@gmail.com>
2025-01-15 23:20:27 +0800

    Add Bastion Host EC2 instance to Terraform configuration

--- Problem_2/main.tf
+++ Problem_2/main.tf
@@ -15,3 +15,9 @@ module "eks" {
 }
 
 
+module "ec2" {
+  source       = "./modules/ec2"
+  project_name = var.project_name
+  vpc_id       = module.vpc.vpc_id
+  subnet_id    = module.vpc.public_subnet_ids
+}
---
+++ Problem_2/modules/ec2/variables.tf
@@ -0,0 +1,23 @@
+
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "project_name" {
+    type = string
+}
+
+variable "ami_id" {
+  type = string
+  default = "ami-07469e8b9deb9b996" 
+}
+
+variable "instance_type" {
+  type = string
+  default = "t3.micro"
+}
+
+variable "subnet_id" {
+  type = list(string)
+}
